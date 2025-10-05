b8da6418fa8c8f3b5c41ae0cb7568a38d3e89bbe
Dimitar Karaskakovski <dimitarkaraskakovski92@gmail.com>
2025-10-05 22:22:25 +0200

    added post route, db logic and data displaying in the htl table

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-@app.route("/")
+    def __repr__(self):
+        return '<Task %r>' % self.id    
+
+
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding your task"
+
+    else:
+        task = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=task)
 
 
 if __name__ == "__main__":
